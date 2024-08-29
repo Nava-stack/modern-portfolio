ca928b1b706d7d35ee522c09e52a3974ddb0bf52
Nava-stack <129419033+Nava-stack@users.noreply.github.com>
2024-08-30 00:39:04 +0530

    refactor: Update BentoGrid styling and add background gradient

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,10 @@ import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex flex-col items-center justify-center overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex flex-col items-center justify-center mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full ">
         <FloatingNav navItems={navItems} />
         <Hero />
