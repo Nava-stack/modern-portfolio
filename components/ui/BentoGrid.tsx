9b1a7ee4382373904fa66d80c957a66148157cdf
Nava-stack <129419033+Nava-stack@users.noreply.github.com>
2024-08-29 13:32:22 +0530

    refactor: Update BentoGrid styling and add background gradient
    The code changes in the BentoGrid component refactor the styling by updating the class names and adding a background gradient. This improves the visual appeal and user experience of the grid items.
    
    This commit also updates the background color of the BentoGridItem component to a linear gradient from rgb(2,0,36) to rgba(0,0,0,1), enhancing the overall design of the grid.

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -45,13 +45,16 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 border-red-500 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4  dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
+      style={{
+        background: "rgb(2,0,36)",
+        backgroundColor:
+          "linear-gradient(180deg, rgba(2,0,36,1) 0%, rgba(0,0,0,1) 100%)",
+      }}
     >
-      {header}
       <div className="group-hover/bento:translate-x-2 transition duration-200">
-        {icon}
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
           {title}
         </div>
